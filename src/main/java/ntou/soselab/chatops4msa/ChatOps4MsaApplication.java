c927e982d8777d8a1e5253e13a147d25d8b8a2c9
Kunishy <149185650+Kunishy@users.noreply.github.com>
2026-04-06 21:40:03 +0800

    prompt 指定英文

--- src/main/java/ntou/soselab/chatops4msa/ChatOps4MsaApplication.java
+++ src/main/java/ntou/soselab/chatops4msa/ChatOps4MsaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatOps4MsaApplication {
+public class -ChatOps4MsaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ChatOps4MsaApplication.class, args);
